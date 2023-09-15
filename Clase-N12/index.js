35a0d13dd302177390b910f109b5fcac772196de
Talia Rodrigues <taliaraodrigues@gmail.com>
2023-09-15 20:06:08 -0300

    lenguaje javascript

---
+++ Clase-N12/index.js
@@ -0,0 +1,50 @@
+//comentario 
+//var fuego;
+
+let entrada = prompt('Ingrese su nombre: ');
+
+alert(entrada);
+
+alert('Hola mundo!');
+
+console.log(entrada);
+
+
+let noombreVariable = ' texto';
+let a = 'abc';
+let b= 5;
+a = 123;
+b = 'def';
+
+console.log(a,b);
+
+var miVarible2 = 'mi variable 2';
+
+var miVarible3 = 'mi variable 3';
+
+console.log(miVarible2,miVarible3);
+
+miVarible2 = 'Cambio de informacion';
+console.log(miVarible2);
+
+let miVarible4 = 'mi variable 4';
+const miVarible5 = 'mi variable 5';
+console.log(miVarible4,miVarible5);
+
+miVarible4 = 'Cambio de informacio2';
+console.log(miVarible4);
+
+
+var dato1 = 'La frase mas larga';
+
+var data2 = 123456;
+
+var suma = 789456;
+
+console.log(data2, suma);
+
+console.log(dato1+ ' ' + data2 + ' ' + suma);
+
+let vacio = null;
+
+let variableNoDefinida = undefined; 
---
+++ Clase-N13/index.js
@@ -0,0 +1,110 @@
+
+// let num1 = 10;
+// let num2 = 5;
+// let suma = num1 + num2;
+// let resta = num1 - num2;
+// let multiplicacion = num1 * num2;
+// let division = num1 / num2;
+
+// console.log("Suma:", suma); // Resultado: 15
+// console.log("Resta:", resta); // Resultado: 5
+// console.log("Multiplicación:", multiplicacion); // Resultado: 50
+// console.log("División:", division); // Resultado: 2
+
+
+// let contador = 5;
+// contador++; // Incremento en 1
+// console.log(contador); // Resultado: 6
+
+
+// let contador1 = 10;
+// contador1--; // Decremento en 1
+// console.log(contador1); // Resultado: 9
+
+
+// let numero = 7;
+// let resultado = ++numero + 5; // Incremento después de la operación
+// console.log(numero); // Resultado: 8 (el valor de 'numero' se incrementa después de la operación)
+// console.log(resultado); // Resultado: 12 (7 + 5)
+
+// let otroNumero = 10;
+// let otroResultado = --otroNumero + 5; // Decremento antes de la operación
+// console.log(otroNumero); // Resultado: 9 (el valor de 'otroNumero' se decrementa antes de la operación)
+// console.log(otroResultado); // Resultado: 14 (9 + 5)
+
+
+
+// let temperatura = 31;
+
+// if (temperatura > 30) {
+//   console.log("Hace calor");
+//   let teclado =5;
+//   console.log(teclado);
+// } else if (temperatura <= 30 && temperatura >= 20) {
+//   console.log("El clima es agradable");
+
+// } else {
+//   console.log("Hace frío");
+// }
+// console.log(teclado);
+
+
+// let diaDeLaSemana = 15;
+// let nombreDia;
+
+// switch (diaDeLaSemana) {
+//   case 1:
+//     nombreDia = "Lunes";
+//     break;
+//   case 2:
+//     nombreDia = "Martes";
+//     break;
+//   case 3:
+//     nombreDia = "Miércoles";
+//     break;
+//   case 4:
+//     nombreDia = "Jueves";
+//     break;
+//   case 5:
+//     nombreDia = "Viernes";
+//     break;
+//   case 6:
+//     nombreDia = "Sábado";
+//     break;
+//   case 7:
+//     nombreDia = "Domingo";
+//     break;
+//   default:
+//     nombreDia = "Día no válido";
+
+// }
+
+// console.log("Hoy es " + nombreDia);
+
+
+
+// let edad = 18;  //prompt('Ingrese su edad:');
+// let mensaje = (edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad";
+// console.log(mensaje); // Resultado: "Eres mayor de edad"
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Iteración #" + (i ));
+//   }
+
+
+
+// let contador = 5;
+
+// while (contador <= 5) {
+//   console.log("Iteración #" + (contador));
+//   contador++;
+// }
+
+
+let contador = 6;
+
+do {
+  console.log("Iteración #" + (contador + 1));
+  contador++;
+} while (contador < 5);
